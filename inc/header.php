794eeb8fd692d88f202e9283a9dcd8bee53be988
wmompie <wmompie@gmail.com>
2019-01-21 23:31:16 -0500

    created index.html with bootstrap and created the php includes

---
+++ inc/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- BOOTSTRAP CSS -->
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+      integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+      crossorigin="anonymous"
+    />
+
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="/styles/main.css" />
+
+    <title>Easterseals Run</title>
+  </head>
+  <body>
+    <!-- NAVBAR -->
+    <nav id="mainNavbar" class="navbar navbar-expand-sm navbar-light shadow fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.html">Logo</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="signup.php">Run Sign-Up</a></li>
+            <li class="nav-item"><a class="nav-link" href="refer.php">Refer-a-Friend</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
