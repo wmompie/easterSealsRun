8f6fd7eecce7b4cce932468e60ed5c07e77bf754
Will Mompie <wmompie@gardco.com>
2019-01-23 12:42:49 -0500

    fixing favicon at work

---
+++ inc/footer.php
@@ -0,0 +1,38 @@
+<hr />
+
+      <div class="row my-3">
+        <div class=" col-sm-12 text-center">
+          <h4>Easterseals</h4>
+          <p>
+            16105 Schaefer Pike, Pompano Beach, Florida 33060 | 800-221-6827 (toll-free) Easterseals and its affiliate
+            organizations are 501(c)(3) nonprofit organizations.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- FOOTER -->
+    <footer class="navbar navbar-expand-sm navbar-light bg-secondary text-light">
+      <div class="container-fluid"><p class="m-auto">&copy;2019 - William Mompie | Easterseals</p></div>
+    </footer>
+
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+      integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+      integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+      crossorigin="anonymous"
+    ></script>
+
+    <!-- CUSTOM JAVASCRIPT -->
+    <script src="scripts/main.js"></script>
+  </body>
+</html>
