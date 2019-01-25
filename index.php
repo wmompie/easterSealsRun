d5d7868de6658b1fda6f77b2272458cadfee28fc
wmompie <wmompie@gmail.com>
2019-01-24 20:12:44 -0500

    pulling git

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- FAVICON -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+
+    <!-- BOOTSTRAP CSS -->
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+      integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+      crossorigin="anonymous"
+    />
+
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="styles/main.css" />
+
+    <title>Easterseals Run</title>
+  </head>
+  <body>
+    <!-- NAVBAR -->
+    <nav id="mainNavbar" class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.html"><img id="logo" src="img/logo.png" alt="easterseals logo"/></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Run Sign-Up</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Refer-a-Friend</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- JUMBOTRON HEADER -->
+    <div id="home-section" class="jumbotron-fluid py-5 bg-light shadow my-5">
+      <div class="dark-overlay"></div>
+      <div class="container text-center text-light">
+        <h1 id="mainHeading" class="display-2">Easterseals Run</h1>
+        <img
+          src="https://images.unsplash.com/photo-1519056312994-33952f238fac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1988&q=80"
+          alt="placeholder image"
+          id="jumbo-img"
+          class="d-none d-sm-inline img-fluid w-75 my-2 rounded"
+        />
+        <div id="jumbo-content">
+          <p class="lead pt-3">
+            The 2019 Miami Easter Seals 1 Mile, 5k and 10k Run/Walk will be held near America Airlines Arena on Sunday,
+            February 24 at 601 Biscayne Blvd in Downtown Miami along Biscayne Bay. The race venue will provide a fun,
+            family-friendly atmosphere, while also delivering a flat, fast and scenic run course through the
+            Metropolitan area.
+          </p>
+          <hr class="my-4" />
+          <p>
+            All events will start and finish at the America Airlines Arena. Registrants receive a t-shirt, finisher’s
+            medal, post-race food and more.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- MAIN SECTION -->
+    <div class="container-fluid">
+      <div class="row justify-content-between px-3">
+        <!-- MAIN CONTENT -->
+        <div class="col-md-7">
+          <div class="border-bottom mb-5">
+            <h2>Easterseals Commitment</h2>
+            <p>
+              For the past 95 years, Easter Seals has played an important role in providing support for children and
+              youth with physical disabilities. Thanks to our generous donors, we continue to offer programs that allow
+              kids to experience freedom, independence and a sense of accomplishment. For families, Easter Seals is
+              somewhere to turn for help when they need it.
+            </p>
+            <p>
+              Easter Seals is a registered charitable organization that helps children and youth with physical
+              disabilities from all ethnic and religious backgrounds. We assist families with the purchase of costly
+              mobility equipment such as wheelchairs, walkers, porch lifts and ramps, as well as communication devices,
+              toileting aids and bathing equipment. Easter Seals also offers post-secondary scholarships and provides
+              access to information and resources, and public education and awareness.
+            </p>
+            <p>Easter Seals helps kids BE KIDS.</p>
+          </div>
+          <div class="mb-4">
+            <p><strong>Event Start Time:</strong> America Airlines Arena, 601 Biscayne Blvd, Miami 33132</p>
+            <h3 class="mt-4">Run/Walk 1 Mile</h3>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">REGISTRATION TYPE</th>
+                  <th scope="col">EFFECTIVE DATES</th>
+                  <th scope="col">ALL AGES</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td scope="row">Early Registration</td>
+                  <td>January 20, 2019 - February 2, 2019</td>
+                  <td>&#36;40</td>
+                </tr>
+                <tr>
+                  <td scope="row">General Registration</td>
+                  <td>February 3, 2019 - February 16, 2019</td>
+                  <td>&#36;45</td>
+                </tr>
+                <tr>
+                  <td scope="row">Late Registration</td>
+                  <td>February 17, 2019 - Race Day</td>
+                  <td>&#36;50</td>
+                </tr>
+              </tbody>
+            </table>
+            <p>&#42;Children 10 and Under are FREE and do not need to be registered.</p>
+            <h3 class="mt-5">Run/Walk 5k or 10k</h3>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">REGISTRATION TYPE</th>
+                  <th scope="col">EFFECTIVE DATES</th>
+                  <th scope="col">ALL AGES</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td scope="row">Early Registration</td>
+                  <td>January 20, 2019 - February 2, 2019</td>
+                  <td>&#36;35</td>
+                </tr>
+                <tr>
+                  <td scope="row">General Registration</td>
+                  <td>February 3, 2019 - February 16, 2019</td>
+                  <td>&#36;40</td>
+                </tr>
+                <tr>
+                  <td scope="row">Late Registration</td>
+                  <td>February 17, 2019 - Race Day</td>
+                  <td>&#36;45</td>
+                </tr>
+              </tbody>
+            </table>
+            <div class="asterisk-section mb-4">
+              <p>&#42;Children 10 and Under are FREE and do not need to be registered.</p>
+              <p>&#42;Children will not receive a race t-shirt. They will be given a finishers medal.</p>
+              <p>
+                &#42;A &#36;5 fee will apply to any race/bib changes after registration is completed. This applies to
+                runners only as they will be timed and a new timing chip will be required.
+              </p>
+            </div>
+            <h3>What Does Your Registration Fee Cover?</h3>
+            <p>
+              The Miami Easter Seals 1 Mile, 5k and 10k Run/Walk is a fundraiser for children and youth with physical
+              disabilities. Your event registration fee helps offset the cost of the event and support the programs and
+              services offered by Easter Seals of South Florida. Your registration fee entitles you to the following:
+            </p>
+            <ul class="mb-4">
+              <li>Event entry</li>
+              <li>Technical t-shirt for participants</li>
+              <li>Chip timing</li>
+              <li>Race number/bib</li>
+              <li>Post-race food</li>
+              <li>Race kit goodie bag</li>
+              <li>Finisher's medal</li>
+            </ul>
+          </div>
+        </div>
+
+        <!-- SIDEBAR -->
+        <div class="col-md-4 border-left d-none d-md-block">
+          <section class="pl-4">
+            <div class="border-bottom mb-5">
+              <h4 class="text-center">Learn More About Easterseals Run</h4>
+              <p>
+                Easterseals Run is a national fundraiser to support the work of Easterseals. For nearly 100 years,
+                Easterseals has been helping people with disabilities and their families, caregivers, veterans and
+                seniors get the services and supports they need to reach their full potential.
+              </p>
+              <p>
+                Individual walkers and teams join hundreds of other passionate people in their communities for a day
+                full of entertainment, fun and, of course, our signature walk! Since 2003, over 230,000 people have
+                participated in 615 Easterseals Run events nationwide generating nearly &#36;41 million to support
+                families at Easterseals. This year, you can help Easterseals accomplish even more!
+              </p>
+              <p>
+                Each participant's motivation to walk is unique; it may be for themselves, a child of a loved one, a
+                neighbor or a colleague. 20&#37; of the US population lives with a disability today- like autism which
+                has become a significant issue for too many families. We walk for them, their future and the families
+                who love them. Easterseals Run to help people with disabilities live, learn, work and play.
+              </p>
+              <p>
+                Every walk event is different -- your walk could be at a local school or at the aquarium! But every walk
+                is a lifeline of funds that enables your local Easterseals to continue providing vital services and
+                support to people with disabilities and their families.
+              </p>
+            </div>
+            <div class="border-bottom mb-5">
+              <h4 class="text-center">How Your Money Helps</h4>
+              <h5>Financial</h5>
+              <p>
+                Children with physical disabilities require costly equipment to meet basic needs, live safely, and
+                strive for independence. The families of these children face not only personal sacrifice, but also a
+                heavy financial burden. It can cost families more than &#36;40,000 a year to care for a child with a
+                severe disability, and as children grow, equipment must be replaced. Government agencies cover only
+                partial costs of certain pieces of equipment necessary to these children for personal safety, mobility,
+                and communication, and for some pieces, not at all.
+              </p>
+              <p>
+                Extra family expenses can include: home renovations, adaptive equipment and devices, special schooling,
+                special transportation and adapted vehicles, time away from work for medical appointments and surgeries,
+                plus the cost of care givers, to name a few.
+              </p>
+              <p>
+                Of the families who requested financial assistance from Easter Seals and self-reported their annual
+                income in 2016, 61&#37; reported a total family income under &#36;60,000. In addition, 45&#37; of
+                families had a total annual household income under &#36;40,000; and 24&#37; had a total family income
+                under &#36;20,000.
+              </p>
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="row mt-3">
+        <div class=" col-sm-12 text-center">
+          <h4>Easterseals</h4>
+          <p>
+            16105 Schaefer Pike, Pompano Beach, Florida 33060 | 800-221-6827 (toll-free) Easterseals and its affiliate
+            organizations are 501(c)(3) nonprofit organizations.
+          </p>
+          <p>
+            <small>All images provided by <a href="https://unsplash.com/">Unsplash.com</a></small>
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- FOOTER -->
+    <footer class="navbar navbar-expand-sm navbar-light bg-light border-top">
+      <div class="container-fluid"><p class="m-auto">&copy;2019 - William Mompie | Easterseals</p></div>
+    </footer>
+
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.min.js"
+      integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+      integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+      integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+      crossorigin="anonymous"
+    ></script>
+
+    <!-- CUSTOM JAVASCRIPT -->
+    <script src="scripts/main.js"></script>
+  </body>
+</html>
