96c7c00750949e1ae2037dc912e41dff2f758e50
wmompie <wmompie@gmail.com>
2019-01-31 23:12:50 -0500

    created and added social media sidebar

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-// $(function() {
-//   $(document).scroll(function() {
-//     var $nav = $('#mainNavbar');
-//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-//   });
-// });
-
-// // INIT TOOLIP
-// $('[data-toggle="tooltip"]').tooltip();
